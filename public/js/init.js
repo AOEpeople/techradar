47326db4c75fc1eb4a0b4da53ac163abdaf4917f
Sophie Brinkmann <sophie.brinkmann@aoe.com>
2025-05-27 10:47:37 +0200

    chore: change chatbot import

--- public/js/init.js
+++ public/js/init.js
@@ -1,23 +1,11 @@
 (function () {
-    const chatbotElement = document.createElement("div");
-    chatbotElement.id = "chatbot";
-    chatbotElement.style =
-        "position: fixed; bottom: 0; right: 0; z-index: 9999; margin-right: 20px; margin-bottom: 20px";
-    document.body.append(chatbotElement);
+  const chatbotElement = document.createElement("div");
+  chatbotElement.id = "chatbot";
+  document.body.append(chatbotElement);
 
-    // initially load chatbot script
-    const chatbot = document.createElement("script");
-    chatbot.src = "/chatbot/chatbot.umd.js";
-    chatbot.type = "text/javascript";
-    chatbot.onload = function() {
-        Chatbot.init({height: 700, width: 400, botName: "Theo"})
-        Chatbot.show();
-    }
-    document.head.append(chatbot);
-
-    // load chatbot styles
-    const chatbotStyles = document.createElement("link");
-    chatbotStyles.rel = "stylesheet";
-    chatbotStyles.href = "/chatbot/style.css";
-    document.head.append(chatbotStyles);
+  // initially load chatbot script
+  const chatbot = document.createElement("script");
+  chatbot.src = "/chatbot/chatbot.umd.js";
+  chatbot.type = "module";
+  document.head.append(chatbot);
 })();
